e3f60dae69e84d640af888e1438fb6f4b8c3abd1
Felix Diaz <pixcompu@outlook.com>
2015-10-07 23:53:38 -0500

    Agragadas mas metodos a ventas, eladeria esta muerta

--- src/CapaNegocio/Gestion/GestorHeladeria.java
+++ src/CapaNegocio/Gestion/GestorHeladeria.java
@@ -1,9 +1,29 @@
 package CapaNegocio.Gestion;
 
+import CapaNegocio.Entidades.Producto;
+
 /**
  *
  * @author PIX
  */
 public class GestorHeladeria {
     
+    public Producto crearProducto(String nombreDeProducto){
+        
+        switch (nombreDeProducto.toLowerCase()) {
+            case "troll":
+                break;
+            case "helado":
+                break;
+            case "paleta":
+                break;
+            case "boli":
+                break;
+            default:
+                throw new AssertionError();
+        }
+        
+        return null;
+        
+    }
 }
--- src/CapaNegocio/Gestion/Venta.java
+++ src/CapaNegocio/Gestion/Venta.java
@@ -1,5 +1,6 @@
 package CapaNegocio.Gestion;
 
+import CapaNegocio.Entidades.Producto;
 import java.util.Date;
 
 /**
@@ -7,16 +8,37 @@ import java.util.Date;
  * @author PIX
  */
 public class Venta {
-    private int IDVenta;
-    private Date fecha;
-    private float monto;
 
-    public Venta(int IDVenta, Date fecha, float monto) {
+    private int IDVenta;
+    private float monto;
+    private int cantidadDeProductos;
+    private Producto productoVendido;
+    private Date fecha;
+
+    public Venta(int IDVenta, float monto, int cantidadDeProductos, Producto productoVendido, Date fecha) {
         this.IDVenta = IDVenta;
-        this.fecha = fecha;
         this.monto = monto;
+        this.cantidadDeProductos = cantidadDeProductos;
+        this.productoVendido = productoVendido;
+        this.fecha = fecha;
     }
-    
+
+    public Producto getProductoVendido() {
+        return productoVendido;
+    }
+
+    public void setProductoVendido(Producto productoVendido) {
+        this.productoVendido = productoVendido;
+    }
+
+    public int getCantidadDeProductos() {
+        return cantidadDeProductos;
+    }
+
+    public void setCantidadDeProductos(int cantidadDeProductos) {
+        this.cantidadDeProductos = cantidadDeProductos;
+    }
+
     public int getIDVenta() {
         return IDVenta;
     }
@@ -28,7 +50,5 @@ public class Venta {
     public float getMonto() {
         return monto;
     }
-    
-    
-    
+
 }
