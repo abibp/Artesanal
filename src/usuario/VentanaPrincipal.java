b3f97c19d312900080295c4d8529c3453c8bdf18
unknown <miguelburgosric@hotmail.com>
2015-10-17 17:19:45 -0500

    Interfaces chidas

--- src/usuario/VentanaPrincipal.java
+++ src/usuario/VentanaPrincipal.java
@@ -1,7 +1,16 @@
 package usuario;
 
+import javax.swing.*;
+
 /**
  * Created by MiguelAngel on 17/10/2015.
  */
 public class VentanaPrincipal {
+    private JTabbedPane tabbedPane1;
+    private JTable table1;
+    private JButton cobrarButton;
+    private JTextField textField1;
+    private JSpinner spinner1;
+    private JButton buscarButton;
+    private JButton salirButton;
 }
