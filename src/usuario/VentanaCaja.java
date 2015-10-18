2130919e49c24af10957e5299245060c348534e0
Astrid Briceño <abp0349@gmail.com[color]>
2015-10-17 19:32:22 -0500

    ventanas

--- src/usuario/VentanaCaja.java
+++ src/usuario/VentanaCaja.java
@@ -5,7 +5,18 @@ import javax.swing.*;
 /**
  * Created by MiguelAngel on 17/10/2015.
  */
-public class VentanaCaja {
-    private JTextField textField1;
+public class VentanaCaja{
+    private JPanel caja;
+    private JLabel titulo;
+    private JTextField campoTexto;
     private JButton registrarButton;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Caja");
+        frame.setContentPane(new VentanaCaja().caja);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/usuario/VentanaInventario.java
+++ src/usuario/VentanaInventario.java
@@ -1,7 +1,12 @@
 package usuario;
 
+import javax.swing.*;
+
 /**
  * Created by MiguelAngel on 17/10/2015.
  */
 public class VentanaInventario {
+    private JTextField textField3;
+    private JButton agregarButton;
+    private JComboBox comboBox1;
 }
